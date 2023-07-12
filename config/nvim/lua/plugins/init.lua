a368a8694aa8a87b907a6bfdf0f6c79ff20d68b1
Jakub Bortlik <jakub.bortlik@phonexia.com>
2023-07-12 03:54:01 +0200

    Move plugins.lua to plugins/init.lua

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -72,8 +72,21 @@ return {
       { mode = "i", "<C-k><C-l>", desc = "Show keymaps" },
     },
   },
-  "mbbill/undotree", -- Show undo history in a tree
-  "vim-scripts/bash-support.vim",
+  {
+    "mbbill/undotree", -- Show undo history in a tree
+    keys = { { "<Leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle UndoTree" } },
+    config = function()
+      vim.g.undotree_WindowLayout = 2
+    end,
+  },
+  {
+    "simnalamburt/vim-mundo",
+    keys = { { "<Leader>U", "<cmd>MundoToggle<cr>", desc = "Toggle Mundo Tree" } },
+    config = function()
+      vim.g.mundo_preview_bottom = 1
+      vim.g.mundo_verbose_graph = 0
+    end,
+  },
   {
     "vim-scripts/bash-support.vim",
     ft = "sh",
@@ -192,92 +205,11 @@ return {
   {
     "ggandor/flit.nvim",
     config = function()
-      require("flit").setup()
-    end,
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    cmd = "Neotree",
-    keys = {
-      {
-        "<leader>t",
-        function()
-          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-        end,
-        desc = "Explorer NeoTree (cwd)",
-      },
-    },
-    deactivate = function()
-      vim.cmd([[Neotree close]])
-    end,
-    init = function()
-      vim.g.neo_tree_remove_legacy_commands = 1
-      if vim.fn.argc() == 1 then
-        local stat = vim.loop.fs_stat(vim.fn.argv(0))
-        if stat and stat.type == "directory" then
-          require("neo-tree")
-        end
-      end
-    end,
-    opts = {
-      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
-      filesystem = {
-        bind_to_cwd = false,
-        follow_current_file = true,
-        hijack_netrw_behavior = "disabled",
-        use_libuv_file_watcher = true,
-      },
-      window = {
-        mappings = {
-          ["<space>"] = { "toggle_node", nowait = true },
-        },
-      },
-      default_component_configs = {
-        indent = {
-          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = "",
-          expander_expanded = "",
-          expander_highlight = "NeoTreeExpander",
-        },
-        git_status = {
-          symbols = {
-            -- Change type
-            added = "",    -- or "✚", but this is redundant info if you use git_status_colors on the name
-            modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-            deleted = "✖", -- this can only be used in the git_status source
-            renamed = "", -- this can only be used in the git_status source
-            -- Status type
-            untracked = "",
-            ignored = "",
-            unstaged = "",
-            staged = "",
-            conflict = "",
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-      vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-      vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-      vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-      vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
-      require("neo-tree").setup(opts)
-      vim.api.nvim_create_autocmd("TermClose", {
-        pattern = "*lazygit",
-        callback = function()
-          if package.loaded["neo-tree.sources.git_status"] then
-            require("neo-tree.sources.git_status").refresh()
-          end
-        end,
-      })
+      require("flit").setup(
+        {
+          clever_repeat = false,
+        }
+      )
     end,
   },
 
