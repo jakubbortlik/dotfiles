848d66f7a7b75b247240a65b9cbc9aa6b0f462b0
Jakub Bortlik <jakub.bortlik@phonexia.com>
2023-11-20 23:29:22 +0100

    feat(nvim): add helper function for visual mode mappings

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -11,7 +11,13 @@ local nmap = function(keys, func, desc)
   vim.keymap.set("n", keys, func, { desc = desc })
 end
 
+-- Simplify visual mode mappings
+local vmap = function(keys, func, desc)
+  vim.keymap.set("v", keys, func, { desc = desc })
+end
+
 M.get_main = get_main
 M.nmap = nmap
+M.vmap = vmap
 
 return M
