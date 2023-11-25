21e853bc63ee6557034a5ec61abec5b25d1d1d1d
Jakub Bortlík <jakub.bortlik@phonexia.com>
2023-11-26 00:25:28 +0100

    feat: transfer nvim config to jakubbortlik/nvim-config

--- config/nvim/after/ftplugin/fugitive.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set("n", "q", "<cmd>close<cr>", { desc = "Close the Git summary window" })
--- config/nvim/after/ftplugin/neotest-output.lua
+++
@@ -1,2 +0,0 @@
-vim.o.wrap = true
-vim.keymap.set("n", "q", "<cmd>close<cr>", { desc = "Close the Neotest Output" })
