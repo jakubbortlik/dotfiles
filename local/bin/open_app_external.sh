ae1890c3f2807199d5282cf7173e21d44562c92f
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-08-08 11:23:06 +0200

    fix(sway): open firefox on workspace 1

--- local/bin/open_app_external.sh
+++ local/bin/open_app_external.sh
@@ -3,7 +3,7 @@
 EXTERNAL_OUTPUT=$(swaymsg -t get_outputs | jq -r '.[] | select(.name | test("^(DP|HDMI)-")) | .name' | head -1)
 
 if [ -n "$EXTERNAL_OUTPUT" ]; then
-  swaymsg "workspace number 3; move workspace to output $EXTERNAL_OUTPUT; exec foot"
+  swaymsg "workspace number $1; move workspace to output $EXTERNAL_OUTPUT; exec $2"
 else
-  swaymsg "workspace number 3; exec foot"
+  swaymsg "workspace number $1; exec $2"
 fi
